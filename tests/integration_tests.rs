de0369c081e88e2254186b123113b36a5638046b
Tom Selfin <selfint@gmail.com>
2022-10-26 21:06:10 +0300

    start rust-analyzer in project directory

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,7 +4,9 @@ use code_depth;
 
 #[test]
 fn connect_to_rust_analyzer() {
+    // start a rust-analyzer server inside our project directory
     let mut server = Command::new("rust-analyzer")
+        .current_dir("..")
         .spawn()
         .expect("failed to start rust-analyzer");
 
