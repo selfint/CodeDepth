a82e900753e3616d0c6268ccf40b39300abe7a7c
selfint <selfint@gmail.com>
2022-10-29 00:20:00 +0300

    connect to rust-analyzer, closes #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,104 @@
-use std::io::Result;
+use lsp_types::{InitializeResult, Url};
+use regex::Regex;
+use serde_json::{json, Value};
+use std::error::Error;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
 
-pub fn init(server: &mut std::process::Child) -> Result<()> {
-    // server
-    //     .stdin
-    //     .as_mut()
-    //     .expect("failed to acquire stdin of server process");
+fn to_json_rpc(msg: &str) -> Vec<u8> {
+    format!("Content-Length: {}\r\n\r\n{}", msg.as_bytes().len(), msg)
+        .as_bytes()
+        .into()
+}
 
-    Ok(())
+async fn read_lsp_msg<R, M>(reader: &mut R) -> Result<M, Box<dyn Error>>
+where
+    R: AsyncRead + std::marker::Unpin,
+    for<'de> M: serde::de::Deserialize<'de>,
+{
+    // read content-length: \d+
+    let re = Regex::new(r"Content-Length: (\d+)\r\n\r\n").unwrap();
+    let mut buf = vec![];
+
+    loop {
+        // get content-length
+        if let Ok(byte) = reader.read_u8().await {
+            buf.push(byte);
+        };
+
+        let text = std::str::from_utf8(&buf).unwrap();
+
+        let content_length = match re.captures(&text) {
+            Some(matches) => match matches.get(1) {
+                None => return Err("failed to extract content-length".into()),
+                Some(content_length) => match content_length.as_str().parse::<usize>() {
+                    Ok(content_length) => Some(content_length),
+                    Err(e) => return Err(Box::new(e)),
+                },
+            },
+            None => None,
+        };
+
+        if content_length.is_none() {
+            continue;
+        }
+
+        // read the rest of the message
+        let expected_msg_left = content_length.unwrap();
+        let mut msg_buf = Vec::with_capacity(expected_msg_left);
+        if let Err(e) = reader.read_buf(&mut msg_buf).await {
+            return Err(Box::new(e));
+        }
+
+        let value: Value = serde_json::from_slice(&msg_buf)?;
+
+        let result = value
+            .get("result")
+            .ok_or("failed to get result from json response")?;
+
+        // TODO: why do we need to clone here?
+        let response = serde_json::from_value::<M>(result.clone())?;
+
+        return Ok(response);
+    }
+}
+
+pub async fn init(
+    server: &mut tokio::process::Child,
+    root_uri: Url,
+) -> Result<InitializeResult, Box<dyn Error>> {
+    let init_msg = lsp_types::InitializeParams {
+        root_uri: Some(root_uri),
+        ..Default::default()
+    };
+    let init_msg_str = serde_json::to_value(&init_msg).expect("failed to serialize init message");
+    let init_msg_json_rpc = json!({
+            "jsonrpc": 2.0,
+            "id": 0,
+            "method": "initialize",
+            "params": init_msg_str
+    });
+    let init_msg_buf = to_json_rpc(&init_msg_json_rpc.to_string());
+
+    let stdin = server
+        .stdin
+        .as_mut()
+        .ok_or("failed to acquire stdin of server process")?;
+
+    let stdout = server
+        .stdout
+        .as_mut()
+        .ok_or("failed to acquire stdout of server process")?;
+
+    let _stderr = server
+        .stderr
+        .as_mut()
+        .ok_or("failed to acquire stderr of server process")?;
+
+    stdin.write_all(&init_msg_buf).await?;
+
+    let msg: InitializeResult = read_lsp_msg(stdout).await?;
+
+    Ok(msg)
 }
 
 #[cfg(test)]
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,16 +1,32 @@
-use std::process::Command;
+use std::{path::Path, process::Stdio};
+
+use lsp_types::Url;
+use tokio::process::Command;
 
 use code_depth;
 
-#[test]
-fn connect_to_rust_analyzer() {
+#[tokio::test]
+async fn connect_to_rust_analyzer() {
     // start a rust-analyzer server inside our project directory
     let mut server = Command::new("rust-analyzer")
         .current_dir("..")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()
         .expect("failed to start rust-analyzer");
 
-    code_depth::init(&mut server).expect("failed to init rust-analyzer");
+    let project_path = Path::new("..")
+        .canonicalize()
+        .expect("failed to canonicalize project path");
+    let project_path = project_path.as_os_str();
 
-    server.kill().expect("failed to stop rust-analyzer");
+    let project_url =
+        Url::from_file_path(project_path).expect("failed to convert project path to URL");
+
+    let response = code_depth::init(&mut server, project_url).await;
+
+    server.kill().await.expect("failed to stop rust-analyzer");
+
+    assert!(response.is_ok());
 }
