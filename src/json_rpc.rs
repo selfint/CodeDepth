cf58a6ea4e2eb23e962f04b1e22653867c6bf777
selfint <selfint@gmail.com>
2022-10-29 13:40:37 +0300

    extract json_rpc module

---
+++ src/json_rpc.rs
@@ -0,0 +1,72 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::error::Error;
+use tokio::io::{AsyncRead, AsyncReadExt};
+
+pub const JSON_RPC_VERSION: f32 = 2.0;
+
+pub fn build_request<T: for<'de> Deserialize<'de> + Serialize>(
+    id: usize,
+    method: &str,
+    params: &Option<T>,
+) -> Vec<u8> {
+    let mut j = json!({
+            "jsonrpc": JSON_RPC_VERSION,
+            "id": id,
+            "method": method,
+    });
+
+    if let Some(params) = params {
+        j["params"] = serde_json::to_value(params).expect("failed to serialize params");
+    }
+
+    let json_str = j.to_string();
+
+    format!("Content-Length: {}\r\n\r\n{}", json_str.len(), json_str)
+        .as_bytes()
+        .into()
+}
+
+pub async fn get_next_response<R>(reader: &mut R) -> Result<Vec<u8>, Box<dyn Error>>
+where
+    R: AsyncRead + std::marker::Unpin,
+{
+    // match content-length: \d+
+    // and also match the separating \r\n\r\n to the actual content
+    // so that when we read the message we just read 'content-length' bytes
+    let re = Regex::new(r"Content-Length: (\d+)\r\n\r\n").unwrap();
+    let mut buf = vec![];
+
+    // get content-length
+    let content_length = loop {
+        let byte = reader.read_u8().await?;
+        buf.push(byte);
+
+        let text = std::str::from_utf8(&buf)?;
+
+        if let Some(matches) = re.captures(&text) {
+            let first_match = matches.get(1).ok_or("failed to extract content-length")?;
+            break first_match.as_str().parse::<usize>()?;
+        }
+    };
+
+    // read the rest of the message
+    let mut msg_buf = Vec::with_capacity(content_length);
+    if let Err(e) = reader.read_buf(&mut msg_buf).await {
+        return Err(Box::new(e));
+    }
+
+    Ok(msg_buf)
+}
+
+#[derive(Debug, PartialEq, Clone, Default, Deserialize)]
+struct Response<T> {
+    pub id: usize,
+    pub jsonrpc: String,
+    pub result: T,
+}
+
+pub fn get_response_result<T: for<'de> Deserialize<'de>>(buf: &[u8]) -> Result<T, Box<dyn Error>> {
+    Ok(serde_json::from_slice::<Response<T>>(&buf)?.result)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,15 @@
+mod json_rpc;
+
 use lsp_types::{InitializeResult, Url};
-use regex::Regex;
-use serde_json::{json, Value};
 use std::error::Error;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 
-fn to_json_rpc(msg: &str) -> Vec<u8> {
-    format!("Content-Length: {}\r\n\r\n{}", msg.as_bytes().len(), msg)
-        .as_bytes()
-        .into()
-}
-
-async fn read_lsp_msg_buf<R>(reader: &mut R) -> Result<Vec<u8>, Box<dyn Error>>
-where
-    R: AsyncRead + std::marker::Unpin,
-{
-    // match content-length: \d+
-    // and also match the separating \r\n\r\n to the actual content
-    // so that when we read the message we just read 'content-length' bytes
-    let re = Regex::new(r"Content-Length: (\d+)\r\n\r\n").unwrap();
-    let mut buf = vec![];
-
-    // get content-length
-    let content_length = loop {
-        let byte = reader.read_u8().await?;
-        buf.push(byte);
-
-        let text = std::str::from_utf8(&buf)?;
-
-        if let Some(matches) = re.captures(&text) {
-            let first_match = matches.get(1).ok_or("failed to extract content-length")?;
-            break first_match.as_str().parse::<usize>()?;
-        }
-    };
-
-    // read the rest of the message
-    let mut msg_buf = Vec::with_capacity(content_length);
-    if let Err(e) = reader.read_buf(&mut msg_buf).await {
-        return Err(Box::new(e));
-    }
-
-    Ok(msg_buf)
-}
-
-fn parse_msg_key<M>(msg_buf: Vec<u8>, key: &str) -> Result<M, Box<dyn Error>>
-where
-    for<'de> M: serde::de::Deserialize<'de>,
-{
-    let value = serde_json::from_slice::<Value>(&msg_buf)?;
-
-    let result = value
-        .get(key)
-        .ok_or(format!("failed to get {} from json response", key))?;
-
-    // TODO: why do we need to clone here?
-    let response = serde_json::from_value::<M>(result.clone())?;
-
-    Ok(response)
-}
+use json_rpc::{build_request, get_next_response, get_response_result};
 
 pub async fn init(
     server: &mut tokio::process::Child,
     root_uri: Url,
 ) -> Result<InitializeResult, Box<dyn Error>> {
-    let init_msg = lsp_types::InitializeParams {
-        root_uri: Some(root_uri),
-        ..Default::default()
-    };
-    let init_msg_str = serde_json::to_value(&init_msg).unwrap();
-    let init_msg_json_rpc = json!({
-            "jsonrpc": 2.0,
-            "id": 0,
-            "method": "initialize",
-            "params": init_msg_str
-    });
-    let init_msg_buf = to_json_rpc(&init_msg_json_rpc.to_string());
-
     let stdin = server
         .stdin
         .as_mut()
@@ -85,16 +20,17 @@ pub async fn init(
         .as_mut()
         .ok_or("failed to acquire stdout of server process")?;
 
-    let _stderr = server
-        .stderr
-        .as_mut()
-        .ok_or("failed to acquire stderr of server process")?;
+    let init_params = lsp_types::InitializeParams {
+        root_uri: Some(root_uri),
+        ..Default::default()
+    };
+    let init_request = build_request(0, "initialize", &Some(init_params));
 
-    stdin.write_all(&init_msg_buf).await?;
+    stdin.write_all(&init_request).await?;
 
-    let msg_buf = read_lsp_msg_buf(stdout).await?;
+    let response = get_next_response(stdout).await?;
 
-    let msg = parse_msg_key::<InitializeResult>(msg_buf, "result")?;
+    let msg = get_response_result(&response)?;
 
     Ok(msg)
 }
