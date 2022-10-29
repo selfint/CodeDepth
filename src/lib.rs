cde92427c18fe1144bbb12486199174e7d0999f9
selfint <40059156+selfint@users.noreply.github.com>
2022-10-29 13:45:35 +0300

    implement init lsp server method (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod json_rpc;
+
+use lsp_types::{InitializeResult, Url};
+use std::error::Error;
+use tokio::io::AsyncWriteExt;
+
+use json_rpc::{build_request, get_next_response, get_response_result};
+
+pub async fn init(
+    server: &mut tokio::process::Child,
+    root_uri: Url,
+) -> Result<InitializeResult, Box<dyn Error>> {
+    let stdin = server
+        .stdin
+        .as_mut()
+        .ok_or("failed to acquire stdin of server process")?;
+
+    let stdout = server
+        .stdout
+        .as_mut()
+        .ok_or("failed to acquire stdout of server process")?;
+
+    let init_params = lsp_types::InitializeParams {
+        root_uri: Some(root_uri),
+        ..Default::default()
+    };
+    let init_request = build_request(0, "initialize", &Some(init_params));
+
+    stdin.write_all(&init_request).await?;
+
+    let response = get_next_response(stdout).await?;
+
+    let msg = get_response_result(&response)?;
+
+    Ok(msg)
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,34 @@
+use std::{path::Path, process::Stdio};
+
+use lsp_types::Url;
+use tokio::process::Command;
+
+use code_depth;
+
+#[tokio::test]
+async fn connect_to_rust_analyzer() {
+    // start a rust-analyzer server inside our project directory
+    let mut server = Command::new("rust-analyzer")
+        .current_dir("..")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("failed to start rust-analyzer");
+
+    let project_path = Path::new("..")
+        .canonicalize()
+        .expect("failed to canonicalize project path");
+    let project_path = project_path.as_os_str();
+
+    let project_url =
+        Url::from_file_path(project_path).expect("failed to convert project path to URL");
+
+    let response = code_depth::init(&mut server, project_url).await;
+
+    server.kill().await.expect("failed to stop rust-analyzer");
+
+    // fail if response is err, but with nice debug info
+    response.unwrap();
+    assert!(true);
+}
