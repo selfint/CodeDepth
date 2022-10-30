367d1117e498dfc241e8c22c04132995541206fb
selfint <selfint@gmail.com>
2022-10-30 19:16:08 +0200

    properly query symbols

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 pub mod json_rpc;
 
-use std::{collections::HashSet, error::Error, hash::Hash, time::Duration};
+use std::{error::Error, time::Duration};
 
 use lsp_types::{InitializeResult, SymbolInformation, Url, WorkspaceSymbolParams};
-use serde::{Deserialize, Serialize};
 use tokio::io::AsyncWriteExt;
 
 use json_rpc::{build_notification, build_request, get_next_response, get_response_result};
-
-const ABC: &str = "abcdefghijklmnopqrstuvwxyz";
 
 pub async fn init<R, W>(
     reader: &mut R,
@@ -41,7 +38,9 @@ where
 
     let response = get_next_response(writer).await?;
 
-    let msg = get_response_result::<InitializeResult>(&response)?;
+    let msg = get_response_result::<InitializeResult>(&response)?
+        .response
+        .unwrap();
 
     let initialized_params = lsp_types::InitializedParams {};
     let initialized_request = build_notification("initialized", &Some(initialized_params));
@@ -53,63 +52,60 @@ where
 pub async fn get_function_definitions<R, W>(
     reader: &mut R,
     writer: &mut W,
+    max_duration: Duration,
 ) -> Result<Vec<SymbolInformation>, Box<dyn Error>>
 where
     R: tokio::io::AsyncWrite + std::marker::Unpin,
     W: tokio::io::AsyncRead + std::marker::Unpin,
 {
-    // iterate over abc to get all symbols
-    // TODO: this is stupid, there must be a better way
+    let retry_sleep_duration = 100;
+    let retry_amount = max_duration.as_millis() / retry_sleep_duration;
+    let mut retries_left = retry_amount;
 
-    let mut symbols = HashSet::new();
+    let params = Some(WorkspaceSymbolParams {
+        // for rust-analyzer we need to append '#' to get function definitions
+        // this might not be good for all LSP servers
+        // TODO: add option to set query string by lsp server, and maybe this is the default?
+        query: "#".into(),
+        ..Default::default()
+    });
 
-    for letter in ABC.chars() {
-        dbg!(letter);
-        let params = Some(WorkspaceSymbolParams {
-            query: letter.into(),
-            ..Default::default()
-        });
+    let request = build_request(1, "workspace/symbol", &params);
 
-        let request = build_request(1, "workspace/symbol", &params);
+    reader.write_all(&request).await?;
+    let response = get_next_response(writer).await?;
+    let mut result = get_response_result::<Vec<lsp_types::SymbolInformation>>(&response)
+        .unwrap()
+        .response;
 
-        dbg!(std::str::from_utf8(&request));
-        std::thread::sleep(Duration::from_millis(1000));
+    // wait for server to index project
+    // TODO: add 'lsp-server-ready' check instead of this hack
+    while let Err(e) = result {
+        // make sure the error just means the server is still indexing
+        assert_eq!(e.code, -32801, "got unexpected error from lsp server");
+        retries_left -= 1;
+        if retries_left == 0 {
+            return Err(format!("max retries exceeded: {:?}", e).into());
+        }
+
+        std::thread::sleep(Duration::from_millis(retry_sleep_duration as u64));
 
         reader.write_all(&request).await?;
-
-        dbg!("sent");
-
         let response = get_next_response(writer).await?;
-
-        dbg!(std::str::from_utf8(&response));
-
-        let result = get_response_result::<Vec<lsp_types::SymbolInformation>>(&response).unwrap();
-
-        let letter_symbols = result
-            .iter()
-            .filter(|&s| s.kind == lsp_types::SymbolKind::FUNCTION)
-            .map(|s| s.to_owned())
-            .collect::<Vec<SymbolInformation>>();
-
-        dbg!(&letter_symbols);
-
-        for symbol in letter_symbols {
-            symbols.insert(HashableSymbolInformation(symbol));
-        }
+        result = get_response_result::<Vec<lsp_types::SymbolInformation>>(&response)
+            .unwrap()
+            .response;
     }
 
-    let symbols = symbols.iter().map(|s| s.0.to_owned()).collect();
+    let symbols = result.unwrap();
 
-    Ok(symbols)
-}
+    let function_definitions = symbols
+        .iter()
+        .filter(|&s| s.kind == lsp_types::SymbolKind::FUNCTION)
+        .map(|s| s.to_owned())
+        .collect::<Vec<SymbolInformation>>();
 
-#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Serialize)]
-pub struct HashableSymbolInformation(SymbolInformation);
-
-impl Hash for HashableSymbolInformation {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        serde_json::to_string(&self.0).unwrap().hash(state);
-    }
+    Ok(function_definitions)
 }
 
 #[cfg(test)]
